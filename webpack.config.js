6e381ada988cb43537aa5f45d80814ef61371a89
Rany ElHousieny <elhousie@gmail.com>
2021-08-27 06:35:59 -0700

    HTML Webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ]
 };
