53fb5c60a426f5bcd968ee71dc40aa660d05ea1b
Rany ElHousieny <elhousie@gmail.com>
2021-08-29 12:27:20 -0700

    After Sass

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,11 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
           'style-loader',
           'css-loader',
+          'sass-loader',
         ],
       },
     ],
