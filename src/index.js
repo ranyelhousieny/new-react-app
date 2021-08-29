fc29c9a7a2ea50e36fb776e305139f41ec1c33da
Rany ElHousieny <elhousie@gmail.com>
2021-08-29 10:47:01 -0700

    Functions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
 
+const user = {
+  name: 'Rany ElHousieny',
+  profile: 'https://rany.tk',
+};
+
+function website(site) {
+  if (site) {
+    return site;
+  }
+  return 'Not Available';
+}
+const template = (
+  <div>
+    <h2>
+      {' '}
+      My Name is {user.name}
+    </h2>
+    <p>
+      Please, visit my profile
+      at {website(user.site)}
+    </p>
+  </div>
+);
+
 ReactDOM.render(
-  <h1>
-    Hello from index.js with
-    React and Babel and CSS in
-    RED
-  </h1>,
+  template,
   document.querySelector(
     '#fromjs'
   )
