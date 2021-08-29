f724eebf919cccaddd4fb123627a885f5ec99594
Rany ElHousieny <elhousie@gmail.com>
2021-08-29 02:52:07 -0700

    Added CSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/styles.css';
 
 ReactDOM.render(
   <h1>
     Hello from index.js with
-    React and Babel
+    React and Babel and CSS in
+    RED
   </h1>,
   document.querySelector(
     '#fromjs'
