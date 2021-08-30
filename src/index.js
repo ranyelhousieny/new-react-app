5609536a4f3e5f45c29c78c3ac178534c5810ed7
Rany ElHousieny <elhousie@gmail.com>
2021-08-29 17:45:45 -0700

    React Events Start

--- src/index.js
+++ src/index.js
@@ -2,31 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
-const user = {
-  name: 'Rany ElHousieny',
-  profile: 'https://rany.tk',
-};
+let count = 0;
 
-function website(site) {
-  if (site) {
-    return site;
-  }
-  return 'Not Available';
-}
 const template = (
   <div>
-    <h1>
-      Adding Sass to Webpack
-    </h1>
-    <h2>
-      {' '}
-      My Name is {user.name}
-    </h2>
-    <p>
-      Please, visit my profile
-      at{' '}
-      {website(user.profile)}
-    </p>
+    <h1>Count: {count}</h1>
+    <button>Add One</button>
   </div>
 );
 
