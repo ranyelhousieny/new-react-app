c65587a3c96a01d74b52f3197c27ed5c9c82e294
Rany ElHousieny <elhousie@gmail.com>
2021-09-03 20:31:22 -0700

    Step1 Empty project

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-  mode: 'development',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ]
-};
